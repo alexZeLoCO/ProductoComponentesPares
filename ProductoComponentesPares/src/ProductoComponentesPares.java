7e9fe9ba9d63d6105081c9a6f53f446c20f99f7b
alexZeLoCO <alejandro.rod.lop@gmail.com>
2020-11-06 10:49:25 +0100

    Segundo Commit

--- ProductoComponentesPares/src/ProductoComponentesPares.java
+++ ProductoComponentesPares/src/ProductoComponentesPares.java
@@ -3,21 +3,20 @@ public class ProductoComponentesPares {
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner (System.in);		//CREAR TECLADO
+		System.out.print("Introduzca el primer valor: ");
 		int n = teclado.nextInt();		//ALMACENAR PRIMER VALOR
 		int contador = 1;		//INICIALIZAR CONTADOR
 		int total = 1;		//INICIALIZAR TOTAL
-		if (n==0) {		//SI N ES 0
-			System.out.print("El primer valor es 0, la suma es 0.");		//OUTPUT
-		} else {		//SI NO
-			while (n!=0) {		//MIENTRAS N NO SEA 0 
-				contador++;		//ACTUALIZAR CONTADOR
-				n = teclado.nextInt();		//PEDIR VALOR
-				if (contador%2==0) {		//SI CONTADOR ES PAR
-					total = total * n;		//MULTIPLICAR VALOR POR TOTAL
-				}
+
+		while (n!=0) {		//MIENTRAS N NO SEA 0 
+			if (contador%2==0) {		//SI CONTADOR ES PAR
+				total = total * n;		//MULTIPLICAR VALOR POR TOTAL
 			}
-					//OUTPUT
-			System.out.printf("La suma de los valores introducidos en posiciones pares es: %d.",total);
+			
+			n = teclado.nextInt();		//PEDIR VALOR
+			contador++;		//ACTUALIZAR CONTADOR
 		}
+		//OUTPUT
+		System.out.printf("La suma de los valores introducidos en posiciones pares es: %d.",total);
 	}
 }
